d0c89da61a8d526fd274f310069d42453ca7c99b
Axl S. Cepeda <asgiraldoc@unal.edu.co>
2023-06-09 12:12:56 -0400

    mainCode

--- scripts/mafft.py
+++ scripts/mafft.py
@@ -23,8 +23,8 @@ def mafftFinal(list_files):
 def mafftHap(list_files):
     for input_file in list_files:
         # Define the MAFFT command as a list of strings
-        mafft_command = ["mafft", "--reorder", "--thread", "30", input_file]
-        outf= str(input_file).split(".")[0] + "_aliHap.fasta"
+        mafft_command = ["mafft", "--thread", "30", input_file]
+        outf= str(input_file).split("-")[0] + "_aliHap.fasta"
         # Use subprocess to run the command and redirect the output to a file
         with open(outf, 'w') as outfile:
             subprocess.run(mafft_command, stdout=outfile)
